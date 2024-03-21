9520a619f24ad5d2fbd1d6292b23a79ed2fcb82b
Thomas Béldi <thbe0404@storsjogymnasiet.se>
2024-03-21 08:49:13 +0100

    V1.2

---
+++ googletest/script.js
@@ -0,0 +1,104 @@
+function gravity() {
+    window.open("https://mrdoob.com/projects/chromeexperiments/google-gravity/", "_blank")
+}
+
+function doImage(element) {
+    element.classList.add('do')
+}
+
+function noDo(element) {
+    element.classList.remove('do')
+}
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'f') {
+    let audioPlayer = document.getElementById('audioPlayer');
+    audioPlayer.cloneNode(true).play();
+    }
+}); 
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'g') {
+    let audioPlayer2 = document.getElementById('audioPlayer2');
+    audioPlayer2.cloneNode(true).play();
+    }
+}); 
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'h') {
+    let audioPlayer3 = document.getElementById('audioPlayer3');
+    audioPlayer3.cloneNode(true).play();
+    }
+});
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'j') {
+    let audioPlayer4 = document.getElementById('audioPlayer4');
+    audioPlayer4.cloneNode(true).play();
+    }
+}); 
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'v') {
+    let audioPlayer5 = document.getElementById('audioPlayer5');
+    audioPlayer5.cloneNode(true).play();
+    }
+});
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'c') {
+    let audioPlayer6 = document.getElementById('audioPlayer6');
+    audioPlayer6.cloneNode(true).play();
+    }
+});
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'x') {
+    let audioPlayer7 = document.getElementById('audioPlayer7');
+    audioPlayer7.cloneNode(true).play();
+    }
+});
+ 
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'z') {
+    let audioPlayer8 = document.getElementById('audioPlayer8');
+    audioPlayer8.cloneNode(true).play();
+    }
+});
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'k') {
+    let audioPlayer9 = document.getElementById('audioPlayer9');
+    audioPlayer9.cloneNode(true).play();
+    }
+});
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'l') {
+    let audioPlayer10 = document.getElementById('audioPlayer10');
+    audioPlayer10.cloneNode(true).play();
+    }
+});
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'b') {
+    let audioPlayer11 = document.getElementById('audioPlayer11');
+    audioPlayer11.cloneNode(true).play();
+    }
+});
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'n') {
+    let audioPlayer12 = document.getElementById('audioPlayer12');
+    audioPlayer12.cloneNode(true).play();
+    }
+}); 
+
+document.addEventListener('keydown', function() {
+    if (event.key.toLowerCase() === 'm') {
+    let audioPlayer13 = document.getElementById('audioPlayer13');
+    audioPlayer13.cloneNode(true).play();
+    }
+});
+
+
